24a363dbaba5201f6dba82b432467defb669105e
Francesco Leonardi <fleonardi86@gmail.com>
2017-10-23 16:39:40 +0100

    added timestamp to saved name on php

--- public/php/saveGif.php
+++ public/php/saveGif.php
@@ -3,5 +3,6 @@
     $img = str_replace('data:image/gif;base64,', '', $img);
     $img = str_replace(' ', '+', $img);
     $data = base64_decode($img);
-    file_put_contents('/gifs/image.gif', $data);
+    $filename = '/gifs/gif'.date('m-d-Y_hia').'gif';
+    file_put_contents($filename, $data);
 ?>
