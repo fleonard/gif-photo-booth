cf46f8c9eb9bf49402f7c5f41232676089c9efd2
Francesco Leonardi <fleonardi86@gmail.com>
2017-10-23 16:41:11 +0100

    used .time() instead of date

--- public/php/saveGif.php
+++ public/php/saveGif.php
@@ -3,6 +3,6 @@
     $img = str_replace('data:image/gif;base64,', '', $img);
     $img = str_replace(' ', '+', $img);
     $data = base64_decode($img);
-    $filename = '/gifs/gif'.date('m-d-Y_hia').'gif';
+    $filename = '/gifs/gif_'.time().'gif';
     file_put_contents($filename, $data);
 ?>
